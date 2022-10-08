f0fc351f4e1e02f3718d580b3e967745bfa52ba8
Sonal Attanayake <sonalattanayake@Sonals-MacBook-Pro.local>
2022-10-08 21:21:11 +0530

    start new frontend design

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,9 @@
 import React from 'react';
-import AdminPanel from './components/admin/panel';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/admin" element={<AdminPanel />} />
-      </Routes>
-    </BrowserRouter>
+  <></>
 
 
   );
